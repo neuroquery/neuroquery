7d684ad23f294926c52ce5ca5aa01e79cf6e1e0b
Jerome Dockes <jerome@dockes.org>
2019-07-08 14:14:12 +0200

    add covariancesmoothing estimator

--- neuroquery/nmf.py
+++ neuroquery/nmf.py
@@ -1,9 +1,10 @@
 import numpy as np
+from scipy import sparse
 
 from sklearn.decomposition import NMF
 
 
-def smoothing_matrix_sqrt(X, n_components=300):
+def _smoothing_matrix_sqrt(X, n_components=300):
     nmf = NMF(
         n_components=n_components, max_iter=200,
         random_state=0, alpha=1e-1, l1_ratio=.1, verbose=1)
@@ -12,3 +13,25 @@ def smoothing_matrix_sqrt(X, n_components=300):
     z = np.linalg.norm(u, axis=0)
     v = z * v
     return v
+
+
+class CovarianceSmoothing(object):
+
+    def __init__(self, n_components=300, p=.1):
+        self.n_components = n_components
+        self.p = p
+
+    def fit(self, X):
+        self.V_ = _smoothing_matrix_sqrt(X, n_components=self.n_components)
+        V_norm = np.linalg.norm(self.V_.dot(self.V_.T), axis=1)
+        zero_norm = V_norm == 0
+        V_norm[zero_norm] = 1
+        self.normalized_V_ = self.V_ / V_norm[:, None]
+        self.normalized_V_[zero_norm] = 0
+        return self
+
+    def transform(self, X):
+        if sparse.issparse(X):
+            X = X.A
+        s = np.einsum('ij,jk,lk', X, self.normalized_V_, self.V_, optimize=True)
+        return self.p * s + (1 - self.p) * X
--- neuroquery/tests/test_nmf.py
+++ neuroquery/tests/test_nmf.py
@@ -1,10 +1,28 @@
 import numpy as np
+from scipy import sparse
+
+import pytest
 
 from neuroquery import nmf
 
 
 def test_smoothing_matrix_sqrt():
     rng = np.random.RandomState(0)
-    x = rng.binomial(3, .3, size=(30, 7))
-    v = nmf.smoothing_matrix_sqrt(x, n_components=5)
-    assert v.shape == (7, 5)
+    X = rng.binomial(3, .3, size=(30, 7))
+    V = nmf._smoothing_matrix_sqrt(X, n_components=5)
+    assert V.shape == (7, 5)
+
+
+def test_covariance_smoothing():
+    rng = np.random.RandomState(0)
+    X = rng.binomial(3, .3, size=(30, 7))
+    op = nmf.CovarianceSmoothing(n_components=5).fit(X)
+    smoothed = op.transform(X)
+    assert smoothed.shape == X.shape
+    assert np.allclose(smoothed, X, rtol=.2, atol=.2)
+    assert np.allclose(
+        1., np.linalg.norm(op.normalized_V_.dot(op.V_.T), axis=1))
+    a = sparse.csr_matrix(((1.,), (0,), (0, 1)), shape=(1, 7))
+    s = op.transform(a)
+    assert np.allclose(
+        s, a.A.ravel() * .9 + .1 * op.normalized_V_.dot(op.V_.T)[0])
