cb65277bbe2d0fb0040b1f059e4dc5a992fa56b8
Jerome Dockes <jerome@dockes.org>
2019-07-08 11:08:36 +0200

    add ridge

---
+++ neuroquery/ridge.py
@@ -0,0 +1,185 @@
+import numpy as np
+
+from scipy import linalg
+
+from sklearn.utils.extmath import safe_sparse_dot
+from sklearn.linear_model import LinearRegression
+
+
+_DEFAULT_ALPHAS = np.logspace(-1, 3, 9)
+
+
+def _preprocess(X, Y, feat_penalty=None):
+    new_X = np.empty((X.shape[0], X.shape[1] + 1), dtype=X.dtype)
+    new_X[:, -1] = 1
+    new_X[:, :-1] = X
+    X_mean = new_X.mean(axis=0)[:-1]
+    np.subtract(new_X[:, :-1], X_mean, out=new_X[:, :-1])
+    if feat_penalty is not None:
+        np.divide(new_X[:, :-1], np.sqrt(feat_penalty), out=new_X[:, :-1])
+    intercept = Y.mean(axis=0)
+    Y = Y - intercept
+    return new_X, X_mean, Y, intercept
+
+
+def _gcv(U, s, Y, intercept, alphas):
+    intercept_dim = U.var(axis=0) < 1e-12
+    errors = np.zeros((len(alphas), Y.shape[0]), dtype=Y.dtype)
+    for i, alpha in enumerate(alphas):
+        t = s**2 / (s**2 + alpha)
+        t[intercept_dim] = 1
+        hat = np.einsum('ij,...j,kj->ik', U, t, U, optimize=True)
+        scale = 1 - np.diag(hat)
+        batch_size = np.maximum(1, Y.shape[1] // 10)
+        for batch in range(0, Y.shape[1], batch_size):
+            target = Y[:, batch: batch + batch_size]
+            err = (target - hat.dot(target)) / scale[:, None]
+            errors[i] += np.sum(err**2, axis=1)
+    return errors
+
+
+def ridge_gcv(X, Y, alphas=_DEFAULT_ALPHAS, feat_penalty=None):
+    X, X_mean, Y, intercept = _preprocess(X, Y, feat_penalty)
+    U, s, Vh = linalg.svd(X, full_matrices=False)
+    V = Vh.T
+    intercept_dim = U.var(axis=0) < 1e-12
+    errors = _gcv(U, s, Y, intercept, alphas)
+    mean_error = errors.mean(axis=1)
+    error_std = np.std(errors, axis=1) / np.sqrt(errors.shape[1])
+    best_alpha = alphas[np.argmin(mean_error)]
+    t = s / (s**2 + best_alpha)
+    t[intercept_dim] = 1
+    M = np.einsum('ij,...j,kj->ik', V, t, U, optimize=True)
+    coef = M.dot(Y)
+    M = M[:-1]
+    fitted_intercept = coef[-1]
+    coef = coef[:-1]
+    if feat_penalty is not None:
+        coef = np.divide(coef, np.sqrt(feat_penalty)[:, None], out=coef)
+        if M is not None:
+            M = np.divide(M, np.sqrt(feat_penalty)[:, None], out=M)
+    intercept -= X_mean.dot(coef)
+    intercept -= fitted_intercept
+    return coef, intercept, best_alpha, mean_error, error_std, M
+
+
+def _get_variance(X, Y, coef, intercept, M):
+    res_var = np.mean((X.dot(coef) + intercept - Y)**2, axis=0)
+    var_filter = np.einsum('ij,ij->i', M, M)
+    return res_var, var_filter
+
+
+class RidgeGCV(LinearRegression):
+    def __init__(self, alphas=_DEFAULT_ALPHAS,
+                 feat_penalty=None, store_M=False):
+        self.alphas = alphas
+        self.feat_penalty = feat_penalty
+        self.store_M = store_M
+
+    def _compute_feat_penalty(self, X, Y):
+        if not hasattr(self, 'feat_penalty_'):
+            self.feat_penalty_ = self.feat_penalty
+
+    def fit(self, X, Y):
+        self._compute_feat_penalty(X, Y)
+        (coef, self.intercept_, self.alpha_, self.loo_errors_,
+         self.error_std_, M) = ridge_gcv(
+             X, Y, alphas=self.alphas,
+             feat_penalty=self.feat_penalty_)
+        self.coef_ = coef.T
+        if M is not None:
+            self._res_var, self._var_filter = _get_variance(
+                X, Y, self.coef_.T, self.intercept_, M)
+        if self.store_M:
+            self.M_ = M
+        return self
+
+    def z_energy(self, use_positive_part=True):
+        var = np.outer(self._var_filter, self._res_var)
+        z = self.coef_.T / np.sqrt(var + var.mean())
+        if use_positive_part:
+            np.clip(z, 0, None, out=z)
+        return (z**2).mean(axis=1)
+
+    def z_maps(self):
+        var = np.outer(self._var_filter, self._res_var)
+        z = self.coef_.T / np.maximum(np.sqrt(var), 1e-24)
+        return z
+
+    def prediction_variance(self, X):
+        XM = np.atleast_2d(safe_sparse_dot(X, self.M_, dense_output=True))
+        XMMtXt = (XM**2).sum(axis=1, keepdims=True)
+        return XMMtXt * self._res_var
+
+    def transform_to_z_maps(self, X):
+        return safe_sparse_dot(
+            X, self.coef_.T, dense_output=True) / np.maximum(
+                np.sqrt(self.prediction_variance(X)), 1e-24)
+
+
+def _linreg_energy(X, Y, alphas, use_positive_part=True):
+    ridge = RidgeGCV(alphas=alphas).fit(X, Y)
+    return ridge.z_energy(use_positive_part=use_positive_part)
+
+
+class AdaptiveRidge(RidgeGCV):
+
+    def __init__(self, alphas=_DEFAULT_ALPHAS,
+                 use_positive_part=True, store_M=False):
+        super().__init__(alphas=alphas, store_M=store_M)
+        self.use_positive_part = use_positive_part
+
+    def _compute_feat_penalty(self, X, Y):
+        z_energy = _linreg_energy(X, Y, self.alphas,
+                                  use_positive_part=self.use_positive_part)
+        self.feat_penalty_ = 1 / np.maximum(
+            z_energy - (z_energy.mean() + 2 * z_energy.std()), .001)
+        self.feat_penalty_ /= self.feat_penalty_.min()
+
+
+class SelectiveRidge(RidgeGCV):
+
+    def __init__(self, alphas=_DEFAULT_ALPHAS,
+                 use_positive_part=True, store_M=False):
+        super().__init__(alphas=alphas, store_M=store_M)
+        self.use_positive_part = use_positive_part
+
+    def fit(self, X, Y):
+        self.original_n_features_ = X.shape[1]
+        adapt = AdaptiveRidge(
+            alphas=self.alphas, use_positive_part=self.use_positive_part
+        ).fit(X, Y)
+        self.kept_features_ = np.arange(X.shape[1])[
+            adapt.feat_penalty_ < adapt.feat_penalty_.max()]
+        self.feat_penalty_ = adapt.feat_penalty_[self.kept_features_]
+        del adapt
+        print('keeping {} features'.format(len(self.kept_features_)))
+        super().fit(X[:, self.kept_features_], Y)
+        return self
+
+    def predict(self, X):
+        X = X[:, self.kept_features_]
+        return safe_sparse_dot(
+            X, self.coef_.T, dense_output=True) + self.intercept_
+
+    def z_maps(self, full=True):
+        var = np.outer(self._var_filter, self._res_var)
+        z = self.coef_.T / np.maximum(np.sqrt(var), 1e-24)
+        if not full:
+            return z
+        full_z = np.zeros((self.original_n_features_, self.coef_.shape[0]))
+        full_z[self.kept_features_] = z
+        return full_z
+
+    def prediction_variance(self, X):
+        X = X[:, self.kept_features_]
+        XM = np.atleast_2d(safe_sparse_dot(X, self.M_, dense_output=True))
+        XMMtXt = (XM**2).sum(axis=1, keepdims=True)
+        return XMMtXt * self._res_var
+
+    def transform_to_z_maps(self, X):
+        pred_variance = self.prediction_variance(X)
+        X = X[:, self.kept_features_]
+        return safe_sparse_dot(
+            X, self.coef_.T, dense_output=True) / np.maximum(
+                np.sqrt(pred_variance), 1e-24)
---
+++ neuroquery/tests/test_ridge.py
@@ -0,0 +1,85 @@
+import itertools
+
+import numpy as np
+from scipy import sparse
+
+from sklearn import datasets
+from sklearn.linear_model import RidgeCV
+from sklearn.model_selection import cross_val_score
+
+import pytest
+
+from neuroquery import ridge
+
+ALPHAS = np.logspace(-6, 6, 13)
+
+
+@pytest.mark.parametrize('noise', [0, 1, 1000])
+@pytest.mark.parametrize('n_samples', [11, 101, 301])
+def test_ridge(noise, n_samples):
+    x, y = datasets.make_regression(
+        n_features=100,
+        n_targets=33,
+        effective_rank=4,
+        bias=100,
+        n_informative=7,
+        shuffle=False,
+        noise=noise,
+        n_samples=n_samples,
+        random_state=1)
+    x += 71
+    y -= 370
+    xx, yy = x.copy(), y.copy()
+    sk_ridge = RidgeCV(alphas=ALPHAS).fit(x, y)
+    reg = ridge.RidgeGCV(alphas=ALPHAS).fit(x, y)
+    assert sk_ridge.alpha_ == reg.alpha_
+    assert np.allclose(reg.coef_, sk_ridge.coef_, atol=1e-7)
+    assert np.allclose(reg.intercept_, sk_ridge.intercept_)
+    assert (x == xx).all()
+    assert (y == yy).all()
+
+
+def test_ridge_dtype():
+    x, y = datasets.make_regression(
+        n_features=3, n_targets=7, n_samples=6, random_state=1)
+    x = np.asarray(x, dtype='float32')
+    y = np.asarray(y, dtype='float32')
+    reg = ridge.RidgeGCV(alphas=ALPHAS).fit(x, y)
+    assert reg.coef_.dtype == 'float32'
+    y = np.asarray(y, dtype='float64')
+    reg = ridge.RidgeGCV(alphas=ALPHAS).fit(x, y)
+    assert reg.coef_.dtype == 'float64'
+
+
+@pytest.mark.parametrize(
+    'regressor', [ridge.RidgeGCV, ridge.AdaptiveRidge, ridge.SelectiveRidge])
+def test_z_maps(regressor):
+    x, y = datasets.make_regression(
+        n_features=9, n_targets=13, effective_rank=7, bias=10, n_informative=5,
+        shuffle=False, noise=5, n_samples=57, random_state=1)
+    test_feat = [1, 3, 7]
+    reg = regressor(store_M=True).fit(x, y)
+    for feat in test_feat:
+        w = sparse.csr_matrix(([1], ([0], [feat])), shape=(1, 9))
+        z1 = reg.transform_to_z_maps(w)[0]
+        z2 = reg.z_maps()[feat]
+        assert np.allclose(z1, z2)
+    if hasattr(reg, 'kept_features_'):
+        z = reg.z_maps(full=False)
+        assert z.shape == (len(reg.kept_features_), 13)
+
+
+@pytest.mark.parametrize(
+    'regressor', [ridge.RidgeGCV, ridge.AdaptiveRidge, ridge.SelectiveRidge])
+def test_predictions(regressor):
+    x, y = datasets.make_regression(
+        n_samples=102, n_informative=5, n_features=91, n_targets=117,
+        effective_rank=9, noise=.5, shuffle=False, random_state=0)
+    reg = regressor()
+    sk_reg = RidgeCV()
+    score = cross_val_score(reg, x, y, cv=5)
+    sk_score = cross_val_score(sk_reg, x, y, cv=5)
+    if regressor == ridge.RidgeGCV:
+        assert np.allclose(score, sk_score)
+    else:
+        assert score.mean() > sk_score.mean()
