4f2471de00cb0d27b059d74e6427af8ff3ca5555
Jerome Dockes <jerome@dockes.org>
2019-07-12 16:47:49 +0200

    iter example

--- examples/make_notebooks.sh
+++ examples/make_notebooks.sh
@@ -1,3 +1,1 @@
 sphx_glr_python_to_jupyter.py plot_using_trained_model.py
-jupyter nbconvert --to notebook --execute plot_using_trained_model.ipynb
-mv plot_using_trained_model.nbconvert.ipynb plot_using_trained_model.ipynb
---
+++ examples/run_notebooks.sh
@@ -0,0 +1,2 @@
+jupyter nbconvert --to notebook --execute plot_using_trained_model.ipynb
+mv plot_using_trained_model.nbconvert.ipynb plot_using_trained_model.ipynb
