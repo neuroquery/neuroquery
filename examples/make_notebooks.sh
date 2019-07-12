2035fc9ae8cd9deae59c02a4dded0c969c50a987
Jerome Dockes <jerome@dockes.org>
2019-07-12 14:49:30 +0200

    add notebook

---
+++ examples/make_notebooks.sh
@@ -0,0 +1,3 @@
+sphx_glr_python_to_jupyter.py plot_using_trained_model.py
+jupyter nbconvert --to notebook --execute plot_using_trained_model.ipynb
+mv plot_using_trained_model.nbconvert.ipynb plot_using_trained_model.ipynb
