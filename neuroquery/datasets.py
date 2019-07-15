9936719f29c52a66988df902d304384105ba911e
Jerome Dockes <jerome@dockes.org>
2019-07-15 15:00:21 +0200

    fix close zip file

--- neuroquery/datasets.py
+++ neuroquery/datasets.py
@@ -19,18 +19,20 @@ def get_neuroquery_data_dir(data_dir=None):
 
 
 def _download_neuroquery_model(out_dir):
+    print('Downloading NeuroQuery model...')
     resp = requests.get(
         'https://raw.githubusercontent.com/neuroquery/neuroquery_data/'
         'master/neuroquery_model.zip')
     resp.raise_for_status()
+    print('Downloaded.')
     with tempfile.TemporaryDirectory() as tmp_dir:
         tmp_dir = pathlib.Path(tmp_dir)
         zip_path = str(tmp_dir / "neuroquery_data_master.zip")
         with open(zip_path, 'wb') as f:
             f.write(resp.content)
-        zip_f = zipfile.ZipFile(zip_path)
-        extract_dir = tmp_dir / "extract_dir"
-        zip_f.extractall(str(extract_dir))
+        with zipfile.ZipFile(zip_path) as zip_f:
+            extract_dir = tmp_dir / "extract_dir"
+            zip_f.extractall(str(extract_dir))
         model_path = extract_dir / "neuroquery_model"
         shutil.copytree(str(model_path), out_dir)
     return out_dir
