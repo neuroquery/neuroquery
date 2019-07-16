8cbc4d2b6dd274e76c049da04b382033f316935c
Jerome Dockes <jerome@dockes.org>
2019-07-16 14:28:38 +0200

    update init

--- neuroquery/__init__.py
+++ neuroquery/__init__.py
@@ -1,3 +1,13 @@
-from . import tokenization
+"""
+NeuroQuery
+----------
 
-__all__ = ["tokenization"]
+Statistical learning package to learn to map text onto brain activity with
+meta-analysis of the neuroimaging literature.
+
+"""
+
+__all__ = [
+    "datasets", "img_utils", "nmf", "ridge", "smoothed_regression",
+    "text_to_brain", "tokenization"
+]
