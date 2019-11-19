ace0a3ef09579db0036b6553c4b4a52c383418bc
Alexandre Pérez <alexperez@wanadoo.fr>
2019-11-19 11:14:12 -0500

    Merged KDA & MKDA examples

--- examples/meta_example.py
+++ examples/meta_example.py
@@ -1,4 +1,4 @@
-# # Meta-analysis: an example of use of the MKDA method.
+# # Meta-analysis: an example of use of the KDA & MKDA method.
 
 # +
 import pandas as pd
@@ -6,7 +6,7 @@ from nilearn import plotting
 
 from neuroquery import datasets
 from neuroquery.img_utils import coordinates_to_arrays
-from neuroquery.meta_analysis import MKDA
+from neuroquery.meta_analysis import KDA, MKDA
 
 # +
 # Load the coordinates from the NeuroQuery dataset.
@@ -20,7 +20,17 @@ affine = masker.mask_img_.affine
 
 # Compute the MKDA maps from the array generator. Since the iter_arrays
 # generate arrays only we have to pass the affine separatly.
+kda_img = KDA(iter_arrays, affine)
+
+# Since the previous iterator has been consulmed, we create a new one
+iter_arrays, masker = coordinates_to_arrays(coordinates)
+affine = masker.mask_img_.affine
+# -
+
+# Compute the MKDA maps from the array generator. Since the iter_arrays
+# generate arrays only we have to pass the affine separatly.
 mkda_img = MKDA(iter_arrays, affine)
 
 # Plot the result
+plotting.view_img(kda_img, threshold=3.0).open_in_browser()
 plotting.view_img(mkda_img, threshold=3.0).open_in_browser()
